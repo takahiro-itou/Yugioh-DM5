8899190175b5e13a0146a3b6f45dcca62a2e9ac8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-08-28 12:57:20 +0900

    作業途中
    (1) パックのカードリストのインクルードをテスト

--- CardList/ReadPackCardList.cpp
+++ CardList/ReadPackCardList.cpp
@@ -1,3 +1,34 @@
 
+enum class  CardRarity
+{
+    COMMON,
+    NORMAL,
+    SECRET,
+    ULTRA,
+    SUPER,
+    RARE,
+    NOTHING,
+};
+
+struct  PackCardList
+{
+    int         number;
+    CardRarity  rarity;
+};
+
 #define     PACK_CARD(number, rarity)   \
-    { number, rarity }
+    { number, CardRarity::rarity }
+
+
+constexpr   PackCardList
+packTable[][64] = {
+    {
+#include    "Vol1.pcl"
+        PACK_CARD( 0, NOTHING )
+    }
+};
+
+int main(int argc, char * argv[])
+{
+    return ( 0 );
+}
