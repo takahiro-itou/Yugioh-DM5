a8b52ca338d2c69dd255465fc30221061804b5ba
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-10-24 22:47:06 +0900

    カードテーブルを初期化

--- CardList/ReadPackCardList.cpp
+++ CardList/ReadPackCardList.cpp
@@ -1,6 +1,19 @@
 
 #include    "PackCardList.h"
 
+constexpr   int     NUM_CARDS   = 928;
+
+int  g_cardTable[NUM_CARDS + 1] = { 0 };
+
+void  initializeCardTable()
+{
+    for ( int i = 0; i <= NUM_CARDS; ++ i ) {
+        g_cardTable[i]  = 0;
+    }
+    for ( int p = 0; p < getArraySize(packTable); ++ p ) {
+    }
+}
+
 int main(int argc, char * argv[])
 {
     return ( 0 );
